affec32e00a875e3ed97740bb757bfad32f6b86b
adnanesaber15 <adnanesaber15@gmail.com>
2021-06-19 20:58:34 +0100

    hh

--- LoginTests/settings.py
+++ LoginTests/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '6!8q2ft2)kpjgt$w=mhejl^5a2j7sm9$ofdmd#5pr^3)g6aq4&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['xerixy.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['xerixy.herokuapp.com', '127.0.0.1']
 
 # Application definition
 django_heroku.settings(locals())
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), ]
 LOGIN_URL = '/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
